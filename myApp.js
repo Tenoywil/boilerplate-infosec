5cd5a4cb0e83da5e11dca0e9ba2a696ab4c717e8
Tenoywil <tenoy.wilson@gmail.com>
2021-03-07 14:34:02 -0500

    helmet tests

--- myApp.js
+++ myApp.js
@@ -1,60 +1,47 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var helmet = require("helmet");
 
+app.use(helmet());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: "deny" }));
+//app.use(helmet());//does all the middlewares below
+app.use(helmet.hidePoweredBy());
+app.use(
+  helmet.frameguard({
+    action: "deny",
+  })
+);
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+let ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+app.use(
+  helmet.hsts({
+    maxAge: ninetyDaysInSeconds,
+    force: true,
+  })
+);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "example.com"],
+      objectSrc: ["'none'"],
+    },
+  })
+);
 module.exports = app;
-var api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+var api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
